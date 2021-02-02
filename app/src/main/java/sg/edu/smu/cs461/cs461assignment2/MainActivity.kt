0123578185e51817a683717fb4962031620777cf
Shaun Phua <shaun.phua.2018@sis.smu.edu.sg>
2021-02-02 13:14:45 +0800

    Add save item to internal storage

--- app/src/main/java/sg/edu/smu/cs461/cs461assignment2/MainActivity.kt
+++ app/src/main/java/sg/edu/smu/cs461/cs461assignment2/MainActivity.kt
@@ -5,6 +5,11 @@ import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import org.w3c.dom.Text
+import java.io.File
+import java.io.PrintStream
+import android.content.Context
+import android.util.Log
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +18,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun addItem(view: View) {
-        val item = findViewById<EditText>(R.id.item)
+        val item = (findViewById<EditText>(R.id.item))
+        val output = PrintStream(openFileOutput("saved_items.txt", MODE_APPEND))
+        output.println(item.text.toString())
+        output.close()
+
+        Toast.makeText(this, "You have added ${item.text} to your to do list!", Toast.LENGTH_SHORT).show()
+        item.setText("")
+
+
     }
 }
