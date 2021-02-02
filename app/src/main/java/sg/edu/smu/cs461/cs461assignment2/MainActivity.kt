c3eed8c0a7897c361d9ffd785b6f02249f60d83c
Gladwin <gladwin.neo.2018@sis.smu.edu.sg>
2021-02-02 12:29:46 +0800

    gradle config

--- app/src/main/java/sg/edu/smu/cs461/cs461assignment2/MainActivity.kt
+++ app/src/main/java/sg/edu/smu/cs461/cs461assignment2/MainActivity.kt
@@ -3,13 +3,22 @@ package sg.edu.smu.cs461.cs461assignment2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.EditText
 import org.w3c.dom.Text
+import sg.edu.smu.cs461.cs461assignment2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var toDoItem = ArrayList<String>()
+    private lateinit var myAdapter: ArrayAdapter<String>
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         setContentView(R.layout.activity_main)
+
     }
 
     fun addItem(view: View) {
