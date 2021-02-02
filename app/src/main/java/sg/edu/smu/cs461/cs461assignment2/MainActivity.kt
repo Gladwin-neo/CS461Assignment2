c5167b5e13b3d19325f97e4f50fac19d5ff6f026
Shaun Phua <shaun.phua.2018@sis.smu.edu.sg>
2021-02-02 12:15:24 +0800

    Add addItem function

--- app/src/main/java/sg/edu/smu/cs461/cs461assignment2/MainActivity.kt
+++ app/src/main/java/sg/edu/smu/cs461/cs461assignment2/MainActivity.kt
@@ -2,10 +2,17 @@ package sg.edu.smu.cs461.cs461assignment2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun addItem(view: View) {
+        val item = findViewById<EditText>(R.id.item)
     }
 }
